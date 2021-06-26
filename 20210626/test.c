bdccacdf6b15d8df6fec8af092f83f057aaf6e4b
wangyujie <1285589294@qq.com>
2021-06-26 22:50:33 +0800

    习题

---
+++ 20210626/test.c
@@ -0,0 +1,29 @@
+// 不允许遍历链表, 在 pos之前插入 
+void LinkListInsertBefore(LinkNode** head, LinkNode* pos, LinkType value)
+{
+    if (head == NULL)
+    {
+        //非法输入
+        return;
+    }
+    if (*head == NULL)
+    {
+        *head = CreateNode(value);
+        return;
+    }
+    if (pos == NULL)
+    {
+        return;
+    }
+    if (pos == *head)
+    {
+        LinkListPushFront(head, value);
+        return;
+    }
+    LinkNode* new_node = CreateNode(pos->data);
+    LinkNode* next = pos->next;
+    pos->next = new_node;
+    new_node->next = next;
+    pos->data = value;
+    return;
+}
