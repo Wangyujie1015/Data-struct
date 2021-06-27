b2b29f13f43063a4241357d77a70c45e9d1f7e09
wangyujie <1285589294@qq.com>
2021-06-27 21:26:05 +0800

    cpp

---
+++ 20210627/test.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+namespace N1
+{
+	int a;
+	void fun1()
+	{
+
+	}
+}
+
+int a = 10;
+int b = 9;
+void fun1()
+{
+	printf("funi()");
+}
+
+int main()
+{
+	
+	cin >> a >> b ;
+	cout << a << " " << b;
+}
